9e301bd3a1aa71079e5447072ab0708b820a05fc
Prasad Rokade <prokade@gmail.com>
2025-06-11 08:47:07 +0530

    .env url is used

--- API/index.js
+++ API/index.js
@@ -16,7 +16,7 @@ const allowedURL=process.env.FORONTEND_URL
 app.use(express.json())
 app.use(cors({
    
-    origin:'https://blog-app-ten-ashy.vercel.app',
+    origin:allowedURL,
     credentials:true,
     methods:['POST','GET','PUT','DELETE']
 
